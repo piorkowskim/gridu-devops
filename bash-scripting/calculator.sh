7f602af988cff87a6e685318676678cd6cd849c4
Milosz Piorkowski <diablu1319@gmail.com>
2023-07-25 16:06:39 +0200

    fixed ;)

--- bash-scripting/calculator.sh
+++ bash-scripting/calculator.sh
@@ -1,29 +1,95 @@
 #!/bin/bash
 
-valid_o(){
-char=$1
-is_valid=1
-
-case "$char" in
-	+ | - | % | \*) is_valid=0 ;;
-esac
-if [ "$is_valid" -ne 0 ]; then
-	echo "Error: Invalid character. Valid ones are: +, -, %, *"
-	exit 1
-fi
+# Function to print usage information
+print_usage() {
+  echo "Usage: $0 -o <operation> -n <numbers> [-d]"
 }
-d=0
 
-while getopts "o:n:d:" opt; do
-	case $opt in
-		o) echo valid_o $OPTARG; option=$OPTARG ;;
-		n) numbers+=($OPTARG) ;;
-		d) d=1 ;;
-	esac
-done
-shift $((OPTIND -1))
+# Initialize variables
+operation=""
+numbers=()
+debug=1
 
-echo "option: " $option "d: " $d
-for val in "${numbers[@]}"; do
-	echo " - $val"
+# Function to print debug information
+print_debug_info() {
+  echo "User: $(whoami)"
+  echo "Script: $0"
+  echo "Operation: $operation"
+  echo "Numbers: ${numbers[*]}"
+}
+
+# Process command-line arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -o)
+      shift
+      operation="$1"
+      ;;
+    -n)
+      shift
+      while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+        numbers+=("$1")
+        shift
+      done
+      ;;
+    -d)
+      debug=0
+      shift
+      ;;
+     +|%|-|\*)
+      operation="$1"
+      shift
+      ;;
+    *)
+      echo "Invalid option: $1"
+      print_usage
+      exit 1
+      ;;
+  esac
 done
+
+# Validate input
+if [[ -z "$operation" || ${#numbers[@]} -lt 2 ]]; then
+  print_usage
+  exit 1
+fi
+
+# Perform the calculation based on the provided operation
+result=0
+case "$operation" in
+  +)
+    for num in "${numbers[@]}"; do
+      ((result += num))
+    done
+    ;;
+  -)
+    result="${numbers[0]}"
+    for ((i = 1; i < ${#numbers[@]}; i++)); do
+      ((result -= numbers[i]))
+    done
+    ;;
+  %)
+    result="${numbers[0]}"
+    for ((i= 1; i < ${#numbers[@]}; i++)); do
+       ((result %= numbers[i]))
+    done
+    ;;
+  \*)
+     result="${numbers[0]}"
+     for ((i = 1; i < ${#numbers[@]}; i++)); do
+        ((result *= numbers[i]))
+     done
+     ;;
+  *)
+    echo "Invalid operation: $operation"
+    print_usage
+    exit 1
+    ;;
+esac
+
+# Output the result
+echo "Result: $result"
+# Print debug information if -d flag is passed
+if [[ $debug -eq 0 ]]; then
+     print_debug_info
+fi
