c7444aca2cc57e6de3d95cde4e8ce07c316476d0
milossh <diablu1319@gmail.com>
2023-08-07 21:23:29 +0200

    fixed task1.5

--- python/exercises/task1.5.py
+++ python/exercises/task1.5.py
@@ -1,27 +1,67 @@
-#!/usr/bin/env python3.6
 import argparse
+import platform
+import psutil
+import socket
 
-# mem: total used, free
-# cpu info: model, core numbers, speed
-# current user
-# system load avg
-# ip address
-# -d for distro, -m for mem, -c for cpu, -u for user info, -l for load avera#ge, -i for ip address
-def cli():
-    parser = argparse.ArgumentParser(
-            prog='PC Info',
-            description='Based on flags, program will provide you with specific info about your PC',
-            epilog='Have fun')
-    parser.add_argument('-d', dest='d', action='store_true')
-    parser.add_argument('-m', dest='m', action='store_true')
-    parser.add_argument('-c', dest='c', action='store_true')
-    parser.add_argument('-u', dest='u', action='store_true')
-    parser.add_argument('-l', dest='l', action='store_true')
-    parser.add_argument('-i', dest='i', action='store_true')
-    args = parser.parse_known_args()
-    dic = "dmculi"
-    for char in dic:
-        if char in args:
-            print(f'{char}')
+def get_distro_info():
+    return platform.dist()
 
-cli()
+def get_memory_info():
+    virtual_memory = psutil.virtual_memory()
+    return virtual_memory.total, virtual_memory.used, virtual_memory.free
+
+def get_cpu_info():
+    cpu_info = {}
+    cpu_info['model'] = platform.processor()
+    cpu_info['core_count'] = psutil.cpu_count(logical=False)
+    cpu_info['speed'] = psutil.cpu_freq().current
+    return cpu_info
+
+def get_user_info():
+    return psutil.users()[0].name
+
+def get_load_average():
+    return psutil.getloadavg()
+
+def get_ip_address():
+    return socket.gethostbyname(socket.gethostname())
+
+# -d distro -m memory -c cpu -u userinfo -l loadavg -i ipaddress
+def main():
+    parser = argparse.ArgumentParser(description="Retrieve system information")
+    parser.add_argument("-d", "--distro", action="store_true", help="Get distro info")
+    parser.add_argument("-m", "--memory", action="store_true", help="Get memory info")
+    parser.add_argument("-c", "--cpu", action="store_true", help="Get CPU info")
+    parser.add_argument("-u", "--user", action="store_true", help="Get user info")
+    parser.add_argument("-l", "--loadavg", action="store_true", help="Get load average")
+    parser.add_argument("-i", "--ip", action="store_true", help="Get IP address")
+    args = parser.parse_args()
+
+    if args.distro:
+        print("Distro Info:", get_distro_info())
+
+    if args.memory:
+        total, used, free = get_memory_info()
+        print("Memory Info:")
+        print(f"Total: {total} bytes")
+        print(f"Used: {used} bytes")
+        print(f"Free: {free} bytes")
+
+    if args.cpu:
+        cpu_info = get_cpu_info()
+        print("CPU Info:")
+        print(f"Model: {cpu_info['model']}")
+        print(f"Core Count: {cpu_info['core_count']}")
+        print(f"Speed: {cpu_info['speed']} MHz")
+
+    if args.user:
+        print("Current User:", get_user_info())
+
+    if args.load:
+        load_avg = get_load_average()
+        print("Load Average:", load_avg)
+
+    if args.ip:
+        print("IP Address:", get_ip_address())
+
+main()
