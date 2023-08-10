5803bf0016037df3093e70216c2ab7dfa7bea869
milossh <diablu1319@gmail.com>
2023-08-10 14:31:44 +0200

    added doc strings and removed my access token

--- python/exercises/task2.py
+++ python/exercises/task2.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3.6
 
-import requests, os, json
-from datetime import datetime
+import requests, json
 
 
 # credentials & URLs
 BASE_URL = "https://api.surveymonkey.com/v3"
-ACCESS_TOKEN = "rrO-QkyQB7hoLX17Fl9GSKyrsbREBSjylicHjJ4l0QxyzMPLFlJnbCIweAovK97MhyPMn74zQBJZVBTQ0codqLQ-EiXov5CKELwTukPPmi4nMw.h20ZMM8U7Q.f8uPHb"
+ACCESS_TOKEN = ""
 
 headers = {
     'Authorization': f'Bearer {ACCESS_TOKEN}',
     'Content-Type': 'application/json'
 }
-def create_survey(survey_title):
+def create_survey(survey_title: str):
+    """
+    Creating new survey
+    Args:
+        survey_title: title of the survey
+    Returns:
+        str: id of created survey or none
+    """
     url = f'{BASE_URL}/surveys'
 
     payload = {
@@ -30,7 +36,14 @@ def create_survey(survey_title):
     return survey_id
 
 
-def create_collector(survey_id):
+def create_collector(survey_id: str):
+    """
+    creating a new collector for specific survey
+    Args:
+        survey_id: id of the given survey
+    Returns:
+        str: id of created collector or none
+    """
     url = f'{BASE_URL}/surveys/{survey_id}/collectors'
     payload = {
         'type': 'email',
@@ -49,7 +62,15 @@ def create_collector(survey_id):
     return collector_id
 
 
-def create_email(survey_id, collector_id):
+def create_email(survey_id: str, collector_id: str):
+    """
+    create a e-mail message for a collector
+    Args:
+        survey_id: id of the survey
+        collector_id: id of the collector
+    Returns:
+        str: id of created e-mail
+    """
     url = f'{BASE_URL}/surveys/{survey_id}/collectors/{collector_id}/messages'
     email_payload = {
         'type': 'invite',
@@ -72,7 +93,17 @@ def send_email(survey_id, email_id, collector_id):
     if send_resp.status_code != 200:
         print(f'Failed to send email. Error: {send_resp.text}')
         return None
-def add_recipients(survey_id, collector_id, email_id, email_list):
+def add_recipients(survey_id: str, collector_id: str, email_id: str, email_list: list):
+    """
+    adding recipients to email message
+    Args:
+        survey_id: id of survey
+        collector_id: id of collector
+        email_id: id of email message
+        email_list: list of recipients
+    Returns:
+        dict: recipients or none
+    """
     url = f'{BASE_URL}/surveys/{survey_id}/collectors/{collector_id}/messages/{email_id}/recipients/bulk'
 
     recipients_payload = {
@@ -84,19 +115,27 @@ def add_recipients(survey_id, collector_id, email_id, email_list):
         print(f'Failed to add recipients. Error: {recipients_resp.text}')
 
     return recipients_payload
-def add_pages_questions(survey_id, pages):
+def add_pages_questions(survey_id: str, pages: dict):
+    """"
+    adding pages & questions to the empty survey
+    Args:
+        survey_id: id of the survey
+        pages: dict with titles & questions
+    Returns
+        none
+    """
     url = f'{BASE_URL}/surveys/{survey_id}/pages'
 
+    p_pos = 1
     # adding pages
     for title, questions in pages.items():
-        p_pos = 1
+        q_pos = 0
         pages_payload = {
             'title': title,
             'position': p_pos
         }
-        p_pos += 1
         pages_resp = requests.post(url, headers=headers, json=pages_payload)
-
+        p_pos += 1
         if pages_resp.status_code != 201:
             print(f'Failed to create page. Error: {pages_resp.text}')
             return None
@@ -105,7 +144,6 @@ def add_pages_questions(survey_id, pages):
 
         # adding questions
         for question_id, question_desc in questions.items():
-            q_pos = 1
             question_payload = {
                 'headings': [{'heading': question_desc['Description']}],
                 'family': 'single_choice', # types are single_choice, multiple_choice, matrix, open_ended etc etc
@@ -113,13 +151,16 @@ def add_pages_questions(survey_id, pages):
                 'position': q_pos,
                 'answers': {'choices': [{'text': answer} for answer in question_desc['Answers']]}
             }
-            q_pos += 1
             question_resp = requests.post(f'{url}/{page_id}/questions', headers=headers, json=question_payload)
+            q_pos += 1
             if question_resp.status_code != 201:
                 print(f'Failed to add question. Error: {question_resp.text}')
                 return None
 
 def main():
+    """
+    main function to work with program, basically manages file i/o
+    """
     with open('questions.txt') as f:
         payload = json.load(f)
     survey_title = list(payload.keys())[0]
@@ -136,7 +177,7 @@ def main():
             if email_id:
                 print(f'Successfully created an email with ID {email_id}.')
                 with open('emails.txt') as f:
-                    emails = f.read().splitlines()
+                    emails = f.readlines()
                     add_recipients(survey_id, collector_id, email_id, emails)
                     send_email(survey_id, email_id, collector_id)
 
